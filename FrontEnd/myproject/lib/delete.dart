9f2f1100c9c89b967fda1c370af9fabb35c74850
Samuel Abatneh <samuelabatneh20@gmail.com>
2022-03-16 13:40:25 +0300

    add files

---
+++ FrontEnd/myproject/lib/delete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeletePage extends StatelessWidget {
+  const DeletePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
