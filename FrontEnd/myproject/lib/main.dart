5c7f643fde606769b2c651d4d0a735f869e8beb1
Samuel Abatneh <samuelabatneh20@gmail.com>
2022-03-17 01:47:46 +0300

    add files

--- FrontEnd/myproject/lib/main.dart
+++ FrontEnd/myproject/lib/main.dart
@@ -6,20 +6,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first app trial"),
-        ),
-        body: Card(
-            child: Column(
-          children: <Widget>[
-            Image.network(
-              'https://picsum.photos/250?image=9',
-            ),
-            Text("This my first app that I have draw thank you every body!"),
-          ],
-        )),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My first app trial"),
       ),
-    );
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text("Add Product"),
+            ),
+          ),
+          Card(
+              child: Column(
+            children: <Widget>[
+              Image.network(
+                'https://picsum.photos/250?image=9',
+              ),
+              Text("This my first app that I have draw thank you every body!"),
+            ],
+          ))
+        ],
+      ),
+    ));
   }
 }
